bfbe9576dd013c0ec153437995590d4bd192f39d
zjkang <zk299@nyu.edu>
2025-07-21 02:04:06 +0800

    update

--- configs/train_config.py
+++ configs/train_config.py
@@ -36,7 +36,7 @@ model_path = "configs/relation_detr/relation_detr_resnet50_800_1333.py"
 # specify a checkpoint folder to resume, or a pretrained ".pth" to finetune, for example:
 # checkpoints/relation_detr_resnet50_800_1333/train/2024-03-22-09_38_50
 # checkpoints/relation_detr_resnet50_800_1333/train/2024-03-22-09_38_50/best_ap.pth
-resume_from_checkpoint = None#'checkpoints/relation_detr_resnet50_800_1333/train/2024-11-02-00_36_11'
+resume_from_checkpoint = 'checkpoints/relation_detr_resnet50_800_1333/train/2024-12-20-05_22_34'
 
 learning_rate = 1e-4  # initial learning rate
 optimizer = optim.AdamW(lr=learning_rate, weight_decay=1e-4, betas=(0.9, 0.999))
